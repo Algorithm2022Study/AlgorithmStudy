09575862bc9c4fe76afc91c07dc818b7c463236f
jifrozen <answldjs1836@naver.com>
2022-09-16 02:29:10 +0900

    Code : 구간_합_구하기

---
+++ jifrozen0110/구간_합_구하기_5.java
@@ -0,0 +1,46 @@
+package 자료구조;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 구간_합_구하기_5 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[][] map = new int[N + 1][N + 1];
+		int[][] S = new int[N + 1][N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				S[i][j] = S[i][j - 1] + S[i - 1][j] - S[i - 1][j - 1] + map[i][j];
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int answer = S[x2][y2] - S[x1 - 1][y2] - S[x2][y1 - 1] + S[x1 - 1][y1 - 1];
+			sb.append(answer);
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
+
+	}
+}
