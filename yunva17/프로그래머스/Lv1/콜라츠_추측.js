aaca25be726f99eb6f876223e5f97c7805921335
yunva17 <yunva17@naver.com>
2022-10-15 03:58:31 +0900

    [Code] 23, 24일차

---
+++ yunva17/프로그래머스/Lv1/콜라츠_추측.js
@@ -0,0 +1,22 @@
+function solution(num) {
+  let answer = 0;
+
+  if (num === 1) {
+    return 0;
+  }
+  while (num !== 1) {
+    if (num % 2 === 0) {
+      num = num / 2;
+      answer++;
+    } else {
+      num = num * 3 + 1;
+      answer++;
+    }
+  }
+
+  if (answer > 500) {
+    return -1;
+  }
+
+  return answer;
+}
