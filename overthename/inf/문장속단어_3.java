6b6ef7b90a86561277bbcfeddc3031f83311a561
sia7997 <sia7997@naver.com>
2022-11-06 00:33:30 +0900

    [Code] 44

--- overthename/inf/문장속단어_3.java
+++ overthename/inf/문장속단어_3.java
@@ -4,16 +4,14 @@ import java.util.*;
 public class Main {
 	public String solution(String str) {
 		String answer="";
+		int len=0;
+		String[] list1 = str.split(" ");
+		for(String s:list1) {
+			if(len<s.length()) {
+				len=s.length();
+				answer=s;
 
-		//대문자는 65~90 소문자 97~122 둘의 차이는 32
-		/*for(char c : str.toCharArray()) {
-			if(Character.isLowerCase(c)) answer+= Character.toUpperCase(c);
-			else answer+= Character.toLowerCase(c);
-		}*/
-
-		for(char c : str.toCharArray()) {
-			if(c>=97&&c<=122) answer+= (char)(c-32);
-			else answer+= (char)(c+32);
+			}
 		}
 
 		return answer;
@@ -22,7 +20,7 @@ public class Main {
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-		String str = kb.next();
+		String str = kb.nextLine(); //문자열이 next이고 문장은 nextLine
 		System.out.print(T.solution(str));
 
 	}
