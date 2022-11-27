eb0490080f8b4bbcb9f8ad6322279dc6281bbce7
onda0_0 <qwen0524@gmail.com>
2022-11-27 13:27:38 +0900

    1일차&2일차_프로그래머스 코딩테스트 풀이

---
+++ onda0-0/AlgorithmPython/Programmers/Lv.0/1&2_배열두배만들기.py
@@ -0,0 +1,10 @@
+# 문제 설명
+# 정수 배열 numbers가 매개변수로 주어집니다. 
+# numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+
+def solution(numbers):
+    answer = []
+    for i in numbers:
+        answer.append(i*2)
+        
+    return answer
