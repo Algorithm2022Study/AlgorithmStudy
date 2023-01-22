0e9cb58c687e7b3911091181667db8136f359541
pbkdpwls <pbkdpwls@naver.com>
2023-01-22 17:28:04 +0900

    code : 시저암호 - mon

---
+++ pbkdpwls/구현/시저암호.py
@@ -0,0 +1,10 @@
+def solution(s, n):
+    answer = ''
+    for i in s :
+        if i.isupper() :
+            answer += chr(ord(i)+n) if ord(i)+n < 91  else chr(ord(i)-26+n)
+        elif i.islower() :
+            answer += chr(ord(i)+n) if ord(i)+n < 123  else chr(ord(i)-26+n)
+        elif i == ' ' :
+            answer += i
+    return answer
