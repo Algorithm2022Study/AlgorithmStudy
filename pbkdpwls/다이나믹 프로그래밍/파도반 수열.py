964245385d9362cb2b24623b36afd7037034e591
pbkdpwls <pbkdpwls@naver.com>
2023-02-05 01:20:10 +0900

    code : 파도반 수열, 백준 silver 3 - 토

---
+++ pbkdpwls/다이나믹 프로그래밍/파도반 수열.py
@@ -0,0 +1,12 @@
+import sys
+r = sys.stdin.readline
+
+N = int(r())
+arr = [int(r()) for _ in range(N)]
+seq = [1, 1, 1, 2, 2]
+
+for i in range(5, max(arr)):
+    seq.append(seq[i-1]+seq[i-5])
+
+for i in arr:
+    print(seq[i-1])
