7bdb459e69ec4f7c392734048e26f22d072ef85b
pbkdpwls <pbkdpwls@naver.com>
2023-03-12 08:48:48 +0900

    refactor : 백준 - 나이트의 이동,bfs

--- pbkdpwls/DFS,BFS/나이트의 이동.py
+++ pbkdpwls/DFS,BFS/나이트의 이동.py
@@ -1,35 +1,29 @@
 import sys
 from collections import deque
 
-def bfs(nx, ny, mx, my):
+def bfs():
     queue = deque()
     queue.append((nx,ny))
     visited[nx][ny] = 1
 
     while queue:
-        a,b = queue.popleft()
+        preX, preY = queue.popleft()
 
-        if a==mx and b==my:
-            print(visited[a][b]-1)
+        if preX==mx and preY==my:
+            print(visited[preX][preY]-1)
 
-        # 이동 좌표 모두 적용
-        for i,j in dxy:
-            tx = a + i
-            ty = b + j
+        for x,y in dxy:
+            dx, dy = preX + x, preY + y
+            if (0 <= dx < num) and (0 <= dy < num) and (visited[dx][dy] == 0):
+                queue.append((dx,dy))
+                visited[dx][dy]=visited[preX][preY] + 1
 
-            # 범위 내에 있고, 방문하기 전이라면
-            if (0<=tx<num) and (0<=ty<num) and not visited[tx][ty]:
-                queue.append((tx,ty))
-                visited[tx][ty] = visited[a][b] + 1
+dxy = [(1,2),(2,1),(-1,-2),(-2,-1),(1,-2),(-1,2),(-2,1),(2,-1)]
 
-# 한번에 이동 가능한 좌표
-dxy = [(1,2),(2,1),(-1,-2),(-2,-1),(-1,2),(1,-2),(-2,1),(2,-1)]
-# 테스트 케이스 수
-n = int(sys.stdin.readline())
-
+n = int(input())
 for i in range(n):
-    num = int(sys.stdin.readline())
+    num = int(input())
     visited = [[0 for _ in range(num)] for _ in range(num)]
-    nx, ny = map(int, input().split()) # 현재 위치
-    mx, my = map(int, input().split()) # 목표 위치
-    bfs(nx, ny, mx, my)
+    nx, ny = map(int, sys.stdin.readline().split())
+    mx, my = map(int, sys.stdin.readline().split())
+    bfs()
